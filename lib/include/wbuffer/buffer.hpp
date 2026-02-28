e7a8fba61754265155d35ae8f1ce6b29cb4a1e9e
wiserin <timofeytsep@gmail.com>
2026-02-28 17:59:26 +0300

    code v0.1(init api)

---
+++ lib/include/wbuffer/buffer.hpp
@@ -0,0 +1,102 @@
+#pragma once // Copyright 2026 wiserin
+#include <cstddef>
+#include <cstdint>
+#include <memory>
+#include <memory_resource>
+
+
+namespace wbuffer {
+
+class WBuffer {
+    uint8_t* data_ = nullptr;
+    std::pmr::memory_resource* resource_ = nullptr;
+    std::unique_ptr<std::pmr::memory_resource> owned_resource_ = nullptr;
+
+    size_t size_ = 0;
+    size_t capacity_ = 0;
+
+ public:
+    class Iterator {
+        uint8_t* ptr_ = nullptr;
+        size_t size_ = 0;
+
+     public:
+        Iterator(uint8_t* ptr, size_t size);
+        
+        Iterator(const Iterator& another) = default;
+        Iterator& operator=(const Iterator& another) = default;
+        Iterator(Iterator&& another) = delete;
+        Iterator& operator=(Iterator&& another) = delete;
+
+        Iterator& operator++();
+        [[nodiscard]] Iterator operator++(int);
+        Iterator& operator--();
+        [[nodiscard]] Iterator operator--(int);
+        Iterator& operator+=(size_t index);
+        [[nodiscard]] Iterator operator+(size_t index) const;
+        Iterator& operator-=(size_t index);
+        [[nodiscard]] Iterator operator-(size_t index) const;
+
+        size_t operator-(const Iterator& another) const;
+
+        [[nodiscard]] bool operator==(const Iterator& another) const;
+        [[nodiscard]] bool operator!=(const Iterator& another) const;
+        [[nodiscard]] bool operator>=(const Iterator& another) const;
+        [[nodiscard]] bool operator<=(const Iterator& another) const;
+        [[nodiscard]] bool operator<(const Iterator& another) const;
+        [[nodiscard]] bool operator>(const Iterator& another) const;
+
+        [[nodiscard]] uint8_t& operator*() const;
+
+        [[nodiscard]] uint8_t& operator[](size_t index) const;
+
+        ~Iterator() = default;
+    };
+
+    WBuffer() = default;
+
+    WBuffer(const WBuffer& another);
+    WBuffer& operator=(const WBuffer& another);
+    WBuffer(WBuffer&& another) noexcept;
+    WBuffer& operator=(WBuffer&& another) noexcept;
+
+    WBuffer(size_t capacity);
+    WBuffer(size_t capacity, std::pmr::memory_resource* resource);
+    WBuffer(size_t capacity, std::pmr::memory_resource&& resource);
+
+    [[nodiscard]] uint8_t& operator[](size_t index);
+    [[nodiscard]] uint8_t operator[](size_t index) const;
+
+    [[nodiscard]] bool operator==(const WBuffer& another) const;
+    [[nodiscard]] bool operator!=(const WBuffer& another) const;
+
+    void Swap(WBuffer& another) noexcept;
+    static void Swap(WBuffer& lhs, WBuffer& rhs) noexcept;
+
+    [[nodiscard]] size_t Size() const;
+
+    Iterator Insert(const Iterator& iter, uint8_t byte);
+    Iterator Insert(const Iterator& iter, size_t count, uint8_t byte);
+    Iterator Insert(const Iterator& iter, const Iterator& begin, const Iterator& end);
+
+    [[nodiscard]] uint8_t& Front();
+    [[nodiscard]] uint8_t Front() const;
+
+    [[nodiscard]] uint8_t& Back();
+    [[nodiscard]] uint8_t Back() const;
+
+    void PushBack(uint8_t byte);
+    void PopBack();
+
+    Iterator Erase(const Iterator& iter);
+    Iterator Erase(const Iterator& begin, const Iterator& end);
+
+    [[nodiscard]] bool Empty();
+    [[nodiscard]] bool Clear();
+
+    void Resize(size_t new_size);
+
+    ~WBuffer();
+};  
+
+} // namespace wbuffer
